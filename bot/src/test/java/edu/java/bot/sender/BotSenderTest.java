e3ba586065ab1c7e5217b1f82a076738b9b978df
BITniki <shakarimovdima@ya.ru>
2024-02-12 18:18:58 +0300

    clean up

--- bot/src/test/java/edu/java/bot/sender/BotSenderTest.java
+++ bot/src/test/java/edu/java/bot/sender/BotSenderTest.java
@@ -4,15 +4,13 @@ import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.SendResponse;
 import edu.java.BotApplicationTests;
+import java.util.concurrent.Executor;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
-
-import java.util.concurrent.Executor;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
