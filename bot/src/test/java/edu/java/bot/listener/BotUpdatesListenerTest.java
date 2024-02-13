d9e6cf4e4461136d94dc466fe6761d5bef424918
BITniki <shakarimovdima@ya.ru>
2024-02-13 20:24:55 +0300

    cover BotUpdatesListener with tests

---
+++ bot/src/test/java/edu/java/bot/listener/BotUpdatesListenerTest.java
@@ -0,0 +1,51 @@
+package edu.java.bot.listener;
+
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Update;
+import edu.java.BotApplicationTests;
+import edu.java.bot.service.UpdatesService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+import java.util.concurrent.Executor;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+class BotUpdatesListenerTest extends BotApplicationTests {
+    @TestConfiguration
+    static class BotUpdatesListenerTestConfig {
+        @Bean
+        public BotUpdatesListener botUpdatesListener(UpdatesService updatesService, Executor executor) {
+            return new BotUpdatesListener(updatesService, executor);
+        }
+    }
+
+    @Autowired
+    private BotUpdatesListener botUpdatesListener;
+    @MockBean
+    private UpdatesService updatesService;
+
+    @Test
+    @DisplayName("Проверим чтобы все апдейты были переданы в сервис")
+    void process() {
+        List<Update> updateList = List.of(
+            mock(Update.class),
+            mock(Update.class),
+            mock(Update.class)
+        );
+
+        var answer = botUpdatesListener.process(updateList);
+
+        assertThat(answer).isEqualTo(UpdatesListener.CONFIRMED_UPDATES_ALL);
+        verify(updatesService, times(3)).processUpdate(any());
+    }
+}
