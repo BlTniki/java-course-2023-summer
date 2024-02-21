36002943ab4b60ece5f22d4f1f7953b522ec0b37
BITniki <shakarimovdima@ya.ru>
2024-02-21 12:51:47 +0300

    make configuration basis

--- scrapper/src/main/java/edu/java/configuration/ClientConfig.java
+++ scrapper/src/main/java/edu/java/configuration/ClientConfig.java
@@ -2,23 +2,25 @@ package edu.java.configuration;
 
 import edu.java.client.github.GitHubClient;
 import edu.java.client.github.GitHubClientRestClient;
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.client.RestClient;
 
-//@Validated
-//@Configuration
-//@ConfigurationProperties(prefix = "client")
-public class ClientConfig {
-//    @Autowired
-//    private GitHub gitHub;
-//    @Bean
-//    public GitHubClient gitHubClient(RestClient.Builder builder) {
-//        System.out.println("token: " + gitHub.getBaseUrl());
-//        var c = new GitHubClientRestClient(builder);
-//        return c;
-//    }
+@Validated
+@ConfigurationProperties(prefix = "client", ignoreUnknownFields = false)
+public record ClientConfig(
+
+    @NotNull GitHub gitHub
+) {
+    @Bean
+    public GitHubClient gitHubClient(RestClient.Builder builder) {
+        System.out.println("roken: " + gitHub.token());
+        return new GitHubClientRestClient(builder);
+    }
+
+    public record GitHub(@NotEmpty String token, @NotEmpty String baseUrl) {
+    }
 }
