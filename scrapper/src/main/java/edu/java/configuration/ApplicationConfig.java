50d6257a2072fa2cfe5c2a6b4d01a48f86f87763
BITniki <shakarimovdima@ya.ru>
2024-02-21 12:51:47 +0300

    make configuration basis

--- scrapper/src/main/java/edu/java/configuration/ApplicationConfig.java
+++ scrapper/src/main/java/edu/java/configuration/ApplicationConfig.java
@@ -1,24 +1,16 @@
 package edu.java.configuration;
 
-import edu.java.client.github.GitHubClient;
-import edu.java.client.github.GitHubClientRestClient;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import java.time.Duration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.client.RestClient;
 
 @Validated
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
 public record ApplicationConfig(
-    @NotEmpty String url
+    @NotNull
+    Scheduler scheduler
 ) {
-    @Bean
-    public GitHubClient gitHubClient(RestClient.Builder builder) {
-        System.out.println("url: " + url);
-        var c = new GitHubClientRestClient(builder);
-        return c;
+    public record Scheduler(boolean enable, @NotNull Duration interval, @NotNull Duration forceCheckDelay) {
     }
-//    public record Scheduler(boolean enable, @NotNull Duration interval, @NotNull Duration forceCheckDelay) {
-//    }
 }
