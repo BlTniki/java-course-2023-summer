32cc7fde94f0035604ad273204b7e1789f46a693
BITniki <shakarimovdima@ya.ru>
2024-02-21 12:25:42 +0300

    tmp commit

--- scrapper/src/main/java/edu/java/configuration/ApplicationConfig.java
+++ scrapper/src/main/java/edu/java/configuration/ApplicationConfig.java
@@ -1,16 +1,24 @@
 package edu.java.configuration;
 
-import jakarta.validation.constraints.NotNull;
-import java.time.Duration;
+import edu.java.client.github.GitHubClient;
+import edu.java.client.github.GitHubClientRestClient;
+import jakarta.validation.constraints.NotEmpty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.client.RestClient;
 
 @Validated
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
 public record ApplicationConfig(
-    @NotNull
-    Scheduler scheduler
+    @NotEmpty String url
 ) {
-    public record Scheduler(boolean enable, @NotNull Duration interval, @NotNull Duration forceCheckDelay) {
+    @Bean
+    public GitHubClient gitHubClient(RestClient.Builder builder) {
+        System.out.println("url: " + url);
+        var c = new GitHubClientRestClient(builder);
+        return c;
     }
+//    public record Scheduler(boolean enable, @NotNull Duration interval, @NotNull Duration forceCheckDelay) {
+//    }
 }
