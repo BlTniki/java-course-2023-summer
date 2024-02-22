edc7133cc9ae31b81f9fe2e6fba3635fef1ad416
BITniki <shakarimovdima@ya.ru>
2024-02-22 23:30:29 +0300

    clean up

--- scrapper/src/test/java/edu/java/client/exception/ClientExceptionTest.java
+++ scrapper/src/test/java/edu/java/client/exception/ClientExceptionTest.java
@@ -1,15 +1,12 @@
 package edu.java.client.exception;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.web.client.HttpClientErrorException;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
