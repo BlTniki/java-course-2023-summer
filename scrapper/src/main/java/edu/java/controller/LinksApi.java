1abe9acbffd8b4a7f072b3832f6123822594eae5
BITniki <shakarimovdima@ya.ru>
2024-02-23 22:47:21 +0300

    add some open api docs

---
+++ scrapper/src/main/java/edu/java/controller/LinksApi.java
@@ -0,0 +1,65 @@
+package edu.java.controller;
+
+import edu.java.controller.model.AddLinkRequest;
+import edu.java.controller.model.ErrorResponse;
+import edu.java.controller.model.LinkResponse;
+import edu.java.controller.model.ListLinksResponse;
+import edu.java.controller.model.RemoveLinkRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@SuppressWarnings("checkstyle:LineLength")
+@Validated
+@RequestMapping("/links")
+public interface LinksApi {
+    @Operation(summary = "Получить все отслеживаемые ссылки")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Ссылки успешно получены", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ListLinksResponse.class))),
+
+        @ApiResponse(responseCode = "400", description = "Некорректные параметры запроса", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))) })
+    @RequestMapping(produces = { "application/json" },
+                    method = RequestMethod.GET)
+   ResponseEntity<ListLinksResponse> getAll(
+            @Parameter(in = ParameterIn.HEADER) @RequestHeader(value = "Tg-Chat-Id") Long tgChatId
+    );
+
+    @Operation(summary = "Добавить отслеживание ссылки")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Ссылка успешно добавлена", content = @Content(mediaType = "application/json", schema = @Schema(implementation = LinkResponse.class))),
+
+        @ApiResponse(responseCode = "400", description = "Некорректные параметры запроса", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))) })
+    @RequestMapping(produces = { "application/json" },
+                    consumes = { "application/json" },
+                    method = RequestMethod.POST)
+    ResponseEntity<LinkResponse> registerLink(
+        @Parameter(in = ParameterIn.HEADER) @RequestHeader(value = "Tg-Chat-Id") Long tgChatId,
+        @Parameter(in = ParameterIn.DEFAULT) @Valid @RequestBody AddLinkRequest addLinkRequest
+    );
+
+    @Operation(summary = "Убрать отслеживание ссылки")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Ссылка успешно убрана", content = @Content(mediaType = "application/json", schema = @Schema(implementation = LinkResponse.class))),
+
+        @ApiResponse(responseCode = "400", description = "Некорректные параметры запроса", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+
+        @ApiResponse(responseCode = "404", description = "Ссылка не найдена", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))) })
+    @RequestMapping(produces = { "application/json" },
+                    consumes = { "application/json" },
+                    method = RequestMethod.DELETE)
+    ResponseEntity<String> deleteLink(
+        @Parameter(in = ParameterIn.HEADER) @RequestHeader(value = "Tg-Chat-Id") Long tgChatId,
+        @Parameter(in = ParameterIn.DEFAULT) @Valid @RequestBody RemoveLinkRequest removeLinkRequest
+    );
+}
--- scrapper/src/main/java/edu/java/controller/LinksController.java
+++ scrapper/src/main/java/edu/java/controller/LinksController.java
@@ -4,70 +4,25 @@ import edu.java.controller.model.AddLinkRequest;
 import edu.java.controller.model.LinkResponse;
 import edu.java.controller.model.ListLinksResponse;
 import edu.java.controller.model.RemoveLinkRequest;
-import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
-@RequestMapping("/links")
-public class LinksController {
-
-    /**
-     * GET /links : Получить все ссылки
-     *
-     * @param tgChatId (Header 'Tg-Chat-Id') (required)
-     * @return Ссылки успешно получены (status code 200)
-     *         or Некорректные параметры запроса (status code 400)
-     */
-    @GetMapping
-    public ResponseEntity<ListLinksResponse> getAll(
-            @RequestHeader(value = "Tg-Chat-Id") Long tgChatId
-        ) {
-        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body(
-            new ListLinksResponse(
-                List.of(),
-                0
-            )
-        );
+public class LinksController implements LinksApi {
+    @Override
+    public ResponseEntity<ListLinksResponse> getAll(Long tgChatId) {
+        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
     }
 
-    /**
-     * Post /links : Добавить ссылку
-     *
-     * @param tgChatId (Header 'Tg-Chat-Id') (required)
-     * @param addLinkRequest (Request body) (required)
-     * @return Ссылки успешно получены (status code 200)
-     *         or Некорректные параметры запроса (status code 400)
-     */
-    @PostMapping
-    public ResponseEntity<LinkResponse> registerLink(
-            @RequestHeader(value = "Tg-Chat-Id") Long tgChatId,
-            @RequestBody AddLinkRequest addLinkRequest
-        ) {
-        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body(null);
+    @Override
+    public ResponseEntity<LinkResponse> registerLink(Long tgChatId, AddLinkRequest addLinkRequest) {
+        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
     }
 
-    /**
-     * Delete /links : Удалить ссылку
-     *
-     * @param tgChatId (Header 'Tg-Chat-Id') (required)
-     * @param removeLinkRequest (Request body) (required)
-     * @return Ссылка успешно убрана (status code 200)
-     *         or Некорректные параметры запроса (status code 400)
-     *         or Ссылка не найдена (status code 404)
-     */
-    @DeleteMapping
-    public ResponseEntity<String> deleteLink(
-            @RequestHeader(value = "Tg-Chat-Id") Long tgChatId,
-            @RequestBody RemoveLinkRequest removeLinkRequest
-        ) {
-        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body(null);
+    @Override
+    public ResponseEntity<String> deleteLink(Long tgChatId, RemoveLinkRequest removeLinkRequest) {
+        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
     }
 }
