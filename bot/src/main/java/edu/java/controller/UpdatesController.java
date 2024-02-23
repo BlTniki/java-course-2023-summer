4a9db7d0aea90a1b6c4cd1f865f729bed862ee03
BITniki <shakarimovdima@ya.ru>
2024-02-23 23:13:34 +0300

    add some open api docs

--- bot/src/main/java/edu/java/controller/UpdatesController.java
+++ bot/src/main/java/edu/java/controller/UpdatesController.java
@@ -3,24 +3,14 @@ package edu.java.controller;
 import edu.java.controller.model.LinkUpdate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/updates")
-public class UpdatesController {
-    /**
-     * POST /tg-chat/{id} : Отправить обновление
-     *
-     * @param linkUpdate (Request body) (required)
-     * @return
-     * Обновление обработано (status code 200)
-     *         or Некорректные параметры запроса (status code 400)
-     */
-    @PostMapping
-    public ResponseEntity<String> receiveUpdates(@RequestBody LinkUpdate linkUpdate) {
-        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body("wololo");
+public class UpdatesController implements UpdatesApi {
+    @Override
+    public ResponseEntity<Void> updatesPost(LinkUpdate body) {
+        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
     }
 }
