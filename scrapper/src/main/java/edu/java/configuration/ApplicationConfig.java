d49e570614fa9d5ea06a34b50144379854ccddcb
BITniki <shakarimovdima@ya.ru>
2024-02-23 11:59:23 +0300

    Refactor logger bean configuration

--- scrapper/src/main/java/edu/java/configuration/ApplicationConfig.java
+++ scrapper/src/main/java/edu/java/configuration/ApplicationConfig.java
@@ -2,7 +2,10 @@ package edu.java.configuration;
 
 import jakarta.validation.constraints.NotNull;
 import java.time.Duration;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
@@ -11,6 +14,11 @@ public record ApplicationConfig(
     @NotNull
     Scheduler scheduler
 ) {
+    @Bean
+    public Logger logger() {
+        return LogManager.getLogger("app");
+    }
+
     public record Scheduler(boolean enable, @NotNull Duration interval, @NotNull Duration forceCheckDelay) {
     }
 }
