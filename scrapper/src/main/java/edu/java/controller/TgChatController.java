596dd83402986bc2c03ec622606669ebfd88f02e
BITniki <shakarimovdima@ya.ru>
2024-02-23 21:55:07 +0300

    fix path mapping

--- scrapper/src/main/java/edu/java/controller/TgChatController.java
+++ scrapper/src/main/java/edu/java/controller/TgChatController.java
@@ -1,13 +1,16 @@
 package edu.java.controller;
 
+import edu.java.exception.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/tg-chat")
+@RestController
+@RequestMapping("/tg-chat")
 public class TgChatController {
 
     /**
@@ -18,7 +21,7 @@ public class TgChatController {
      *         or Некорректные параметры запроса (status code 400)
      */
     @PostMapping("/{id}")
-    public ResponseEntity<String> registerChat(@PathVariable long id) {
+    public ResponseEntity<String> registerChat(@PathVariable long id) throws Exception {
         return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body("wololo");
     }
 
