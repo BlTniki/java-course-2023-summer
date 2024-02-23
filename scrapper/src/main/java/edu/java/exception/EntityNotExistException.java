f97ccdae05984809202cce4d5929d1940eb36716
BITniki <shakarimovdima@ya.ru>
2024-02-23 21:39:51 +0300

    rename exception

--- scrapper/src/main/java/edu/java/exception/EntityNotExistException.java
+++
@@ -1,7 +0,0 @@
-package edu.java.exception;
-
-public class EntityNotExistException extends RuntimeException {
-    public EntityNotExistException(String message) {
-        super(message);
-    }
-}
