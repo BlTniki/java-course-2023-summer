6d41ed3122e4094569f2d8fa04734724ddec2470
BITniki <shakarimovdima@ya.ru>
2024-02-23 11:59:23 +0300

    Refactor logger bean configuration

--- scrapper/src/main/java/edu/java/configuration/ClientConfig.java
+++ scrapper/src/main/java/edu/java/configuration/ClientConfig.java
@@ -6,7 +6,6 @@ import edu.java.client.stackoverflow.StackOverflowClient;
 import edu.java.client.stackoverflow.StackOverflowClientWebClient;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -20,11 +19,6 @@ public record ClientConfig(
     @NotNull GitHub gitHub,
     @NotNull StackOverflow stackOverflow
 ) {
-    @Bean
-    public Logger logger() {
-        return LogManager.getLogger("client");
-    }
-
     @Bean
     public GitHubClient gitHubClient(WebClient.Builder builder, Logger logger) {
         builder.baseUrl(
