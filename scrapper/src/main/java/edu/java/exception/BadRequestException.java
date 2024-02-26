b1bd2045b376ccf9b74ee8a9153821b36048ed03
BITniki <shakarimovdima@ya.ru>
2024-02-26 18:36:02 +0300

    clean up

--- scrapper/src/main/java/edu/java/exception/BadRequestException.java
+++ scrapper/src/main/java/edu/java/exception/BadRequestException.java
@@ -1,16 +1,14 @@
 package edu.java.exception;
 
 import edu.java.controller.model.ErrorCode;
+import lombok.Getter;
 
+@Getter
 public class BadRequestException extends RuntimeException {
     private final ErrorCode errorCode;
 
     public BadRequestException(String message, ErrorCode errorCode) {
         super(message);
         this.errorCode = errorCode;
-    }
-
-    public ErrorCode getErrorCode() {
-        return errorCode;
     }
 }
