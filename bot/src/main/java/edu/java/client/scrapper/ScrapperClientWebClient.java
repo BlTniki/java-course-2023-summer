68db39832db003348092f037027be97570ec0b38
BITniki <shakarimovdima@ya.ru>
2024-02-26 18:39:47 +0300

    clean up

--- bot/src/main/java/edu/java/client/scrapper/ScrapperClientWebClient.java
+++ bot/src/main/java/edu/java/client/scrapper/ScrapperClientWebClient.java
@@ -11,7 +11,6 @@ import java.net.URI;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatusCode;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
