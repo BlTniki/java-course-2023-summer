ff3f8b81997bb2fe9d697983bb196e3908ec9aaa
BITniki <shakarimovdima@ya.ru>
2024-03-05 15:52:50 +0300

    declare db schema

---
+++ migrations/init_tables.sql
@@ -0,0 +1,25 @@
+CREATE IF NOT EXISTS TABLE chat {
+    id BIGINT NOT NULL,
+
+    PRIMARY KEY (id),
+    UNIQUE (id)
+}
+
+CREATE IF NOT EXISTS TABLE link {
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    url TEXT NOT NULL,
+    last_update TIMESTAMP WITH TIMEZONE,
+
+    PRIMARY KEY (id),
+    UNIQUE (url)
+}
+
+CREATE IF NOT EXISTS TABLE link_subscription {
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    chat_id BIGINT NOT NULL,
+    link_id BIGINT NOT NULL,
+    alias VARCHAR(10),
+
+    PRIMARY KEY (id),
+    UNIQUE (chat_id, link_id, alias)
+};
