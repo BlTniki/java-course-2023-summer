fc805a2c138c8a6649e7755b810053d486d76a02
BITniki <shakarimovdima@ya.ru>
2024-03-08 19:13:34 +0300

    cover JdbcChatDao with tests

--- scrapper/src/test/java/edu/java/domain/dao/JdbcChatDaoTest.java
+++ scrapper/src/test/java/edu/java/domain/dao/JdbcChatDaoTest.java
@@ -1,12 +1,14 @@
 package edu.java.domain.dao;
 
 import edu.java.ScrapperApplicationTests;
+import edu.java.domain.dao.chat.JdbcChatDao;
 import edu.java.domain.dto.ChatDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Transactional
@@ -27,14 +29,44 @@ class JdbcChatDaoTest extends ScrapperApplicationTests {
     }
 
     @Test
+    @DisplayName("Проверим что мы получаем все чаты")
+    @Rollback
     void findAll() {
+        var expected = List.of(
+            new ChatDto(1L),
+            new ChatDto(2L),
+            new ChatDto(3L)
+        );
+        for (var chat: expected) {
+            chatDao.add(chat);
+        }
+
+        var actual = chatDao.findAll();
+
+        assertThat(actual)
+            .containsExactlyInAnyOrderElementsOf(expected);
     }
 
     @Test
+    @DisplayName("Проверим что запись работает")
+    @Rollback
     void add() {
+        var expectedDto = new ChatDto(1L);
+
+        var actualDto = chatDao.add(expectedDto);
+
+        assertThat(actualDto).isEqualTo(expectedDto);
     }
 
     @Test
+    @DisplayName("Проверим что запись удаляется")
+    @Rollback
     void remove() {
+        var expected = List.of();
+        chatDao.add(new ChatDto(1L));
+
+        chatDao.remove(1L);
+
+        assertThat(chatDao.findAll()).isEmpty();
     }
 }
