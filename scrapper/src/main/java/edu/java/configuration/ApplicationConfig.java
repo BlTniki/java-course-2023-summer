a56dbc7da055e8cc2d8aabb75ed62787491af0f9
BITniki <shakarimovdima@ya.ru>
2024-03-10 22:51:38 +0300

    fix

--- scrapper/src/main/java/edu/java/configuration/ApplicationConfig.java
+++ scrapper/src/main/java/edu/java/configuration/ApplicationConfig.java
@@ -3,8 +3,6 @@ package edu.java.configuration;
 import edu.java.LinkUpdaterScheduler;
 import jakarta.validation.constraints.NotNull;
 import java.time.Duration;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.validation.annotation.Validated;
@@ -16,13 +14,8 @@ public record ApplicationConfig(
     Scheduler scheduler
 ) {
     @Bean
-    public Logger logger() {
-        return LogManager.getLogger("app");
-    }
-
-    @Bean
-    public LinkUpdaterScheduler linkUpdateScheduler(Logger logger) {
-        return new LinkUpdaterScheduler(logger);
+    public LinkUpdaterScheduler linkUpdateScheduler() {
+        return new LinkUpdaterScheduler();
     }
 
     public record Scheduler(boolean enable, @NotNull Duration interval, @NotNull Duration forceCheckDelay) {
