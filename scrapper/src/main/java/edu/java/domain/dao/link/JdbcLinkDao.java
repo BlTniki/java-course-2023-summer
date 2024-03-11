579b5d087210284c648c05ed4fcc821a0037f732
BITniki <shakarimovdima@ya.ru>
2024-03-11 17:33:17 +0300

    implement LinkDao

--- scrapper/src/main/java/edu/java/domain/dao/link/JdbcLinkDao.java
+++ scrapper/src/main/java/edu/java/domain/dao/link/JdbcLinkDao.java
@@ -1,9 +1,11 @@
 package edu.java.domain.dao.link;
 
+import edu.java.domain.dao.rowMapper.LinkDtoRowMapper;
 import edu.java.domain.dto.LinkDto;
 import java.net.URI;
 import java.time.OffsetDateTime;
 import java.util.List;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,35 +14,44 @@ import org.springframework.transaction.annotation.Transactional;
 public class JdbcLinkDao implements LinkDao {
     private static final String FIND_ALL_QUERY = "SELECT * FROM link";
     private static final String FIND_BY_ID_QUERY = "SELECT * FROM link WHERE id = ?";
-//    private static final String FIND_BY_URL_QUERY = "SELECT * FROM LI";
+    private static final String FIND_BY_URL_QUERY = "SELECT * FROM link WHERE url = ?";
+    private static final String FIND_BY_LAST_UPDATE_QUERY = "SELECT * FROM link WHERE last_update >= ?";
+    private static final String ADD_QUERY = "INSERT INTO link (url, last_update) VALUES (?, ?) RETURNING *";
+    private static final String REMOVE_QUERY = "DELETE FROM link WHERE id = ? RETURNING *";
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcLinkDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @Override
     public List<LinkDto> findAll() {
-        return null;
+        return jdbcTemplate.query(FIND_ALL_QUERY, new LinkDtoRowMapper());
     }
 
     @Override
     public LinkDto findById(long id) {
-        return null;
+        return jdbcTemplate.queryForObject(FIND_BY_ID_QUERY, new LinkDtoRowMapper(), id);
     }
 
     @Override
     public LinkDto findByUrl(URI uri) {
-        return null;
+        return jdbcTemplate.queryForObject(FIND_BY_ID_QUERY, new LinkDtoRowMapper(), uri);
     }
 
     @Override
     public List<LinkDto> findFromLastUpdate(OffsetDateTime lastUpdate) {
-        return null;
+        return jdbcTemplate.query(FIND_BY_LAST_UPDATE_QUERY, new LinkDtoRowMapper(), lastUpdate);
     }
 
     @Override
     public LinkDto add(LinkDto link) {
-        return null;
+        return jdbcTemplate.queryForObject(ADD_QUERY, new LinkDtoRowMapper(), link.url(), link.lastUpdate());
     }
 
     @Override
     public LinkDto remove(long id) {
-        return null;
+        return jdbcTemplate.queryForObject(REMOVE_QUERY, new LinkDtoRowMapper(), id);
     }
 }
---
+++ scrapper/src/main/java/edu/java/domain/dao/rowMapper/LinkDtoRowMapper.java
@@ -0,0 +1,18 @@
+package edu.java.domain.dao.rowMapper;
+
+import edu.java.domain.dto.LinkDto;
+import java.net.URI;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.OffsetDateTime;
+import org.springframework.jdbc.core.RowMapper;
+
+public class LinkDtoRowMapper implements RowMapper<LinkDto> {
+    @Override
+    public LinkDto mapRow(ResultSet rs, int rowNum) throws SQLException {
+        var id = rs.getLong("id");
+        URI url = rs.getObject("url", URI.class);
+        OffsetDateTime lastUpdate = rs.getObject("last_update", OffsetDateTime.class);
+        return null;
+    }
+}
