284effe3368e167ddbef91cea5637bc445345f26
BITniki <shakarimovdima@ya.ru>
2024-03-15 19:16:45 +0300

    add @NotNull constraint to AddLinkRequest

--- scrapper/src/main/java/edu/java/controller/model/AddLinkRequest.java
+++ scrapper/src/main/java/edu/java/controller/model/AddLinkRequest.java
@@ -1,8 +1,9 @@
 package edu.java.controller.model;
 
+import jakarta.validation.constraints.NotNull;
 import java.net.URI;
 
 public record AddLinkRequest(
-    URI link,
-    String alias
+    @NotNull URI link,
+    @NotNull String alias
 ) {}
