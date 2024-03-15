ce5242a19c9e1a35cdea565fcb6e4ad7927503f0
BITniki <shakarimovdima@ya.ru>
2024-03-15 13:47:28 +0300

    Add javadoc in Link

--- scrapper/src/main/java/edu/java/service/link/model/Link.java
+++ scrapper/src/main/java/edu/java/service/link/model/Link.java
@@ -2,6 +2,9 @@ package edu.java.service.link.model;
 
 import java.net.URI;
 
+/**
+ * Данная модель является комбинацией link и subscription из БД.
+ */
 public record Link(
     long id,
     URI link,
