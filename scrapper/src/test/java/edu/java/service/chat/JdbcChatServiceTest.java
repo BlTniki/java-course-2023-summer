c17a6a457b19a3c8e7356a75b26d933dfbb931fb
BITniki <shakarimovdima@ya.ru>
2024-03-17 16:03:04 +0300

    cover JdbcChatService with tests

---
+++ scrapper/src/test/java/edu/java/service/chat/JdbcChatServiceTest.java
@@ -0,0 +1,59 @@
+package edu.java.service.chat;
+
+import edu.java.ScrapperApplicationTests;
+import edu.java.domain.dao.chat.ChatDao;
+import edu.java.domain.dto.ChatDto;
+import edu.java.service.exception.EntityAlreadyExistException;
+import edu.java.service.exception.EntityNotFoundException;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class JdbcChatServiceTest extends ScrapperApplicationTests {
+    @Autowired
+    private ChatDao chatDao;
+    @Autowired
+    private JdbcChatService jdbcChatService;
+
+    @Test
+    void addChat_ShouldAddChat_WhenChatDoesNotExist() {
+        long chatId = 1L;
+        when(chatDao.findById(anyLong())).thenReturn(Optional.empty());
+
+        jdbcChatService.addChat(chatId);
+
+        verify(chatDao).add(new ChatDto(chatId));
+    }
+
+    @Test
+    void addChat_ShouldThrowEntityAlreadyExistException_WhenChatAlreadyExists() {
+        long chatId = 1L;
+        when(chatDao.findById(anyLong())).thenReturn(Optional.of(new ChatDto(chatId)));
+
+        assertThatThrownBy(() -> jdbcChatService.addChat(chatId))
+            .isInstanceOf(EntityAlreadyExistException.class);
+    }
+
+    @Test
+    void removeChat_ShouldRemoveChat_WhenChatExists() {
+        long chatId = 1L;
+        when(chatDao.findById(anyLong())).thenReturn(Optional.of(new ChatDto(chatId)));
+
+        jdbcChatService.removeChat(chatId);
+
+        verify(chatDao).remove(chatId);
+    }
+
+    @Test
+    void removeChat_ShouldThrowEntityNotFoundException_WhenChatDoesNotExist() {
+        long chatId = 1L;
+        when(chatDao.findById(anyLong())).thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> jdbcChatService.removeChat(chatId))
+            .isInstanceOf(EntityNotFoundException.class);
+    }
+}
