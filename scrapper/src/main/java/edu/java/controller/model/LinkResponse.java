0a2bae6e60d8d97ab1a824f748db284670b70888
BITniki <shakarimovdima@ya.ru>
2024-03-17 15:14:43 +0300

    add from service Link model builder into Link Response type

--- scrapper/src/main/java/edu/java/controller/model/LinkResponse.java
+++ scrapper/src/main/java/edu/java/controller/model/LinkResponse.java
@@ -1,9 +1,15 @@
 package edu.java.controller.model;
 
+import edu.java.service.link.model.Link;
+import jakarta.validation.constraints.NotNull;
 import java.net.URI;
 
 public record LinkResponse(
     long id,
     URI link,
     String alias
-) {}
+) {
+    public static @NotNull LinkResponse from(@NotNull Link link) {
+        return new LinkResponse(link.id(), link.link(), link.alias());
+    }
+}
