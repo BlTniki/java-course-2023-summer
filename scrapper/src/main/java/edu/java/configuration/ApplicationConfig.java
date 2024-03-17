a5096eb4ee11cf8c1a496446d4e8ac642c08fe09
BITniki <shakarimovdima@ya.ru>
2024-03-17 14:59:18 +0300

    add LinkParser Bean

--- scrapper/src/main/java/edu/java/configuration/ApplicationConfig.java
+++ scrapper/src/main/java/edu/java/configuration/ApplicationConfig.java
@@ -1,6 +1,9 @@
 package edu.java.configuration;
 
 import edu.java.LinkUpdaterScheduler;
+import edu.java.service.link.LinkParser;
+import edu.java.service.link.github.GithubLinkParser;
+import edu.java.service.link.stackoverflow.StackOverflowLinkParser;
 import jakarta.validation.constraints.NotNull;
 import java.time.Duration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -16,6 +19,14 @@ public record ApplicationConfig(
     @Bean
     public LinkUpdaterScheduler linkUpdateScheduler() {
         return new LinkUpdaterScheduler();
+    }
+
+    @Bean
+    public LinkParser linkParser() {
+        return LinkParser.link(
+            new GithubLinkParser(),
+            new StackOverflowLinkParser()
+        );
     }
 
     public record Scheduler(boolean enable, @NotNull Duration interval, @NotNull Duration forceCheckDelay) {
