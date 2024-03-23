ccfadb9a2aa38f13da3e1c0f1b21bcb8ccb1d5ac
BITniki <shakarimovdima@ya.ru>
2024-03-23 22:28:54 +0300

    fix tests

--- bot/src/test/java/edu/java/BotApplicationTests.java
+++ bot/src/test/java/edu/java/BotApplicationTests.java
@@ -1,9 +1,11 @@
 package edu.java;
 
+import edu.java.bot.controller.UpdatesController;
 import edu.java.configuration.ApplicationConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
@@ -12,6 +14,8 @@ import org.springframework.test.context.TestPropertySource;
 @TestPropertySource(locations = "classpath:application-test.yml")
 @ActiveProfiles("test")
 public class BotApplicationTests {
+    @MockBean
+    public UpdatesController updatesController;
     @Test
     void contextLoads() {
     }
