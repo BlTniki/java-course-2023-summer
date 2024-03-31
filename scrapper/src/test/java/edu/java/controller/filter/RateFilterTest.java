491586c8293f55ae81ade74ac892c90bccc87223
BITniki <shakarimovdima@ya.ru>
2024-03-31 21:29:12 +0300

    make RateFilter work properly in proxy case

--- scrapper/src/test/java/edu/java/controller/filter/RateFilterTest.java
+++ scrapper/src/test/java/edu/java/controller/filter/RateFilterTest.java
@@ -7,12 +7,15 @@ import io.github.bucket4j.ConsumptionProbe;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.Vector;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.web.servlet.HandlerExceptionResolver;
-
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class RateFilterTest extends ScrapperApplicationTests {
 
@@ -45,7 +48,23 @@ public class RateFilterTest extends ScrapperApplicationTests {
     }
 
     @Test
-    public void testDoFilter() throws Exception {
+    public void testDoFilter_withProxy() throws Exception {
+        Vector<String> v = new Vector<>();
+        v.add("ahhh");
+        v.add("Why there no Enumeration.of()");
+        v.add("Or at least Vector.of");
+        when(request.getHeaders(RateFilter.X_FORWARDED_FOR)).thenReturn(v.elements());
+        when(rateLimiterService.resolveBucket(anyString())).thenReturn(bucket);
+        when(bucket.tryConsumeAndReturnRemaining(1)).thenReturn(probe);
+        when(probe.isConsumed()).thenReturn(true);
+
+        rateFilter.doFilter(request, response, filterChain);
+
+        verify(filterChain).doFilter(request, response);
+    }
+
+    @Test
+    public void testDoFilter_withoutProxy() throws Exception {
         when(request.getRemoteAddr()).thenReturn("127.0.0.1");
         when(rateLimiterService.resolveBucket(anyString())).thenReturn(bucket);
         when(bucket.tryConsumeAndReturnRemaining(1)).thenReturn(probe);
