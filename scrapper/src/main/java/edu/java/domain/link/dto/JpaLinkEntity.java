9a281556ecd6ee8a1681a1dc315a95af366ce506
BITniki <shakarimovdima@ya.ru>
2024-04-06 23:43:55 +0300

    fix URI to String mapping in JpaLinkEntity

--- scrapper/src/main/java/edu/java/domain/link/dto/JpaLinkEntity.java
+++ scrapper/src/main/java/edu/java/domain/link/dto/JpaLinkEntity.java
@@ -11,7 +11,12 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
+import java.io.Serializable;
 import java.net.URI;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
 import java.time.OffsetDateTime;
 import java.util.Map;
 import lombok.AllArgsConstructor;
@@ -19,6 +24,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.Type;
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
+import org.hibernate.usertype.UserType;
 
 @Getter
 @Setter
@@ -32,6 +39,7 @@ public class JpaLinkEntity {
     @Column(name = "id", unique = true, nullable = false)
     private Long id;
 
+    @Type(UriUserType.class)
     @Column(columnDefinition = "text", name = "url", unique = true, nullable = false)
     private URI url;
 
@@ -46,4 +54,77 @@ public class JpaLinkEntity {
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "last_check")
     private OffsetDateTime lastCheck;
+
+    public static class UriUserType implements UserType<URI> {
+        @Override
+        public int getSqlType() {
+            return Types.LONGVARCHAR;
+        }
+
+        @Override
+        public Class<URI> returnedClass() {
+            return URI.class;
+        }
+
+        @Override
+        public boolean equals(URI x, URI y) {
+            if (x == y) {
+                return true;
+            }
+            if (x == null || y == null) {
+                return false;
+            }
+            return x.equals(y);
+        }
+
+        @Override
+        public int hashCode(URI uri) {
+            return uri.hashCode();
+        }
+
+        @Override
+        public URI nullSafeGet(
+            ResultSet resultSet,
+            int i,
+            SharedSessionContractImplementor sharedSessionContractImplementor,
+            Object o
+        ) throws SQLException {
+            String uriString = resultSet.getString(i);
+            return uriString == null ? null : URI.create(uriString);
+        }
+
+        @Override
+        public void nullSafeSet(
+            PreparedStatement preparedStatement,
+            URI uri,
+            int i,
+            SharedSessionContractImplementor sharedSessionContractImplementor
+        ) throws SQLException {
+            if (uri == null) {
+                preparedStatement.setNull(i, getSqlType());
+            } else {
+                preparedStatement.setString(i, uri.toString());
+            }
+        }
+
+        @Override
+        public URI deepCopy(URI uri) {
+            return URI.create(uri.toString());
+        }
+
+        @Override
+        public boolean isMutable() {
+            return false;
+        }
+
+        @Override
+        public Serializable disassemble(URI uri) {
+            return uri.toString();
+        }
+
+        @Override
+        public URI assemble(Serializable serializable, Object o) {
+            return URI.create((String) serializable);
+        }
+    }
 }
